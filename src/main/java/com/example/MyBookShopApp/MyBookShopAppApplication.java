720cdb4545df31fa0425f025fec6cdd9e4f58a6b
alexbranavitckiy <alex.branavitckiy@gmail.com>
2022-01-09 22:59:39 +0300

    init commit

--- src/main/java/com/example/MyBookShopApp/MyBookShopAppApplication.java
+++ src/main/java/com/example/MyBookShopApp/MyBookShopAppApplication.java
@@ -3,10 +3,19 @@ package com.example.MyBookShopApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 @SpringBootApplication
 public class MyBookShopAppApplication {
 
 	public static void main(String[] args) {
+
+
+
+
 		SpringApplication.run(MyBookShopAppApplication.class, args);
 	}
 
--- src/main/java/com/example/MyBookShopApp/config/LocaleChangeConfig.java
+++ src/main/java/com/example/MyBookShopApp/config/LocaleChangeConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @Configuration
 public class LocaleChangeConfig implements WebMvcConfigurer {
