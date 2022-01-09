791bd1df3c3ed0d1d0cfb6964b32e0f3ad18a638
alexbranavitckiy <alex.branavitckiy@gmail.com>
2022-01-09 11:31:45 +0300

    init commit

---
+++ src/main/java/com/example/MyBookShopApp/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.MyBookShopApp.repository;
+
+
+import com.example.MyBookShopApp.data.book.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book,Integer> {
+
+
+
+
+
+}
---
+++ src/main/java/com/example/MyBookShopApp/services/BookService.java
@@ -0,0 +1,14 @@
+package com.example.MyBookShopApp.services;
+
+
+import com.example.MyBookShopApp.data.book.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+
+    List<Book> getBooksData();
+
+
+}
